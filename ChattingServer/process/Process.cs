f37bed5e887896e42f6e3ddccbb50cf5b8fb33b6
차경훈 <cksuwjr@naver.com>
2022-08-24 15:21:53 +0900

    init, first chapter

---
+++ ChattingServer/process/Process.cs
@@ -0,0 +1,18 @@
+﻿
+using Nettention.Proud;
+
+namespace ChattingServer.process
+{
+    internal class CommonProcess
+    {
+        static S2C.Proxy S2CProxy = new S2C.Proxy();
+        static C2S.Stub C2SStub = new C2S.Stub();
+
+        public void InitStub()
+        {
+            ServerLauncher.NetServer.AttachProxy(S2CProxy);
+            ServerLauncher.NetServer.AttachStub(C2SStub);
+        }
+
+    }
+}
